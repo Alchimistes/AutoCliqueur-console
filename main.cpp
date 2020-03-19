2486dd33795a22f8b8e0610205dcdd72814a05a8
agariy <agariy@protonmail.ch>
2020-03-19 18:18:54 +0100

    Update auto cliqueur position mouse

--- main.cpp
+++ main.cpp
@@ -17,20 +17,19 @@ using namespace std;
 int main()
 {
     //constants
-    POINT pt;
     const SHORT Mask = 32768;
     bool start = NULL;
 
     while(1){
 
 
-        GetCursorPos(&pt);
+
         if(GetKeyState(0x41) & Mask){
 
             start = TRUE;
              while(start){
-                usleep(700);
-                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, pt.x, pt.y, 0, 0);
+                usleep(500);
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0,0, 0, 0);
 
                 if(GetKeyState(VK_MENU) & Mask){
                     break;
